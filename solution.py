16abf10b56d4e119c6ad3eeeebf091be0c50a352
morris-alan <61065567+morris-alan@users.noreply.github.com>
2020-02-14 14:40:29 -0800

    Add solution file
    File in which task will be performed.

---
+++ solution.py
@@ -0,0 +1,13 @@
+"""
+  Author: Add your name here.
+  Date : Add the date here.
+  Python-Version: 3.6
+  About : example solution for pluralsight interview project: Finding invalid fake ID numbers.
+"""
+import csv
+
+def main():
+    print("Hello, world.")
+
+if __name__ == "__main__":
+    main()
